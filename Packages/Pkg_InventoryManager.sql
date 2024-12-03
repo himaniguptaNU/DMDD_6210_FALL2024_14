8128195a9edf4f3136350bd00cb97ef336266340
himaniguptaNU <gupta.him@northeastern.edu>
2024-12-03 11:53:05 -0500

    Update Pkg_InventoryManager.sql

--- Packages/Pkg_InventoryManager.sql
+++ Packages/Pkg_InventoryManager.sql
@@ -18,3 +18,29 @@ CREATE OR REPLACE PACKAGE InventoryManager AUTHID DEFINER AS
 END InventoryManager;
 /
 -- End of the package specification
+
+-- Start of the package body
+CREATE OR REPLACE PACKAGE BODY InventoryManager AS
+    PROCEDURE ReplenishStock (
+        p_product_id IN NUMBER,
+        p_warehouse_id IN NUMBER,
+        p_quantity IN NUMBER
+    ) IS
+    BEGIN
+    -- Validate access
+        IF NOT ValidateUserAccess('INVENTORYMANAGER') THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Insufficient privileges to access the InventoryManager package.');
+        END IF;
+        
+        UtilityPackage.ReplenishStock(p_product_id, p_warehouse_id, p_quantity);
+    END ReplenishStock;
+
+    PROCEDURE CheckLowStock IS
+    BEGIN
+    -- Validate access
+        IF NOT ValidateUserAccess('INVENTORYMANAGER') THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Insufficient privileges to access the InventoryManager package.');
+        END IF;
+    
+        UtilityPackage.CheckLowStock;
+    END CheckLowStock;
