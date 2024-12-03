cacde3f55205f3e02c14d019dadcdc68eee57ceb
shalinedward <shalinedward.f@northeastern.edu>
2024-12-03 11:45:24 -0500

    Created the package body of supplier_manager

--- Packages/Pkg_SupplierManager.sql
+++ Packages/Pkg_SupplierManager.sql
@@ -5,3 +5,20 @@ CREATE OR REPLACE PACKAGE SupplierManager AUTHID DEFINER AS
 END SupplierManager;
 /
 -- End of the package specification
+
+----------------------------------------------------------------------------------------------------------------
+-- Start of the package body
+CREATE OR REPLACE PACKAGE BODY SupplierManager AS
+    -- Placeholder package body
+    PROCEDURE CheckLowStock IS
+    BEGIN
+    -- Validate access
+        IF NOT ValidateUserAccess('SUPPLIERMANAGER') THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Insufficient privileges to access the SupplierManager package.');
+        END IF;
+        
+        UtilityPackage.CheckLowStock;
+    END CheckLowStock;
+END SupplierManager;
+/
+-- End of the package body
